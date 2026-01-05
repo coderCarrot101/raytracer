a7ada1d6e7c7371482a328cf0d8e9843743c44e5
codercarrot101 <elijahp08@icloud.com>
2026-01-05 14:11:05 -0600

    LOL IDK IMA SAVE THIS BUILD

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-#include "color.h"
-#include "vec3.h"
-
+/*#include <SDL3/SDL.h>
 #include <iostream>
+
+
 
 int main() {
 
@@ -12,6 +12,8 @@ int main() {
 
     // Render
 
+    //string = << "P3\n" << image_width << ' ' << image_height
+    //live_render(string)
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
@@ -19,8 +21,65 @@ int main() {
         for (int i = 0; i < image_width; i++) {
             auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
             write_color(std::cout, pixel_color);
+            //also write to the function
         }
     }
 
     std::clog << "\rDone.                 \n";
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << SDL_GetError() << "\n";
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow(
+        "SDL3 Window",
+        800, 600,
+        SDL_WINDOW_RESIZABLE
+    ); 
+
+    SDL_Delay(2000);
+    std::clog << "RAN!";
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}*/
+#include <SDL3/SDL.h>
+#include <iostream>
+
+int main()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init failed: " << SDL_GetError() << "\n";
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow(
+        "SDL3 Test Window",
+        800, 600,
+        SDL_WINDOW_RESIZABLE
+    );
+
+    if (!window) {
+        std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << "\n";
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                running = false;
+            }
+        }
+
+        SDL_Delay(16); // ~60 FPS idle
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
